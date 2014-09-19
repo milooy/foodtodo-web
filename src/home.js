48e56ecf840a4894a0b98346e359612587e988e4
yurim <milooy@icloud.com>
2014-09-19 16:43:07 +0900

    blink blink

--- src/home.js
+++ src/home.js
@@ -178,3 +178,155 @@ app.controller('TodoController', function($window, indexedDBDataCon){
   
   init();
 });
+
+
+/////todomon움직임/////
+///*
+ var images = {};
+ var totalResources = 2;
+ var numResourcesLoaded = 0;
+ var fps = 30;
+ 
+ var context = document.getElementById('canvas').getContext("2d");
+ var charX = 30;
+ var charY = 20;
+ 
+
+ 
+ loadImage("todomon-body");
+ loadImage("todomon-leg");
+ 
+ function loadImage(name) {
+	 images[name] = new Image();
+	 images[name].onload = function() {
+		 resourceLoaded();
+	 }
+	 images[name].src = "../img/todomon/" + name + ".png";
+ }
+ 
+
+ 
+ function resourceLoaded() {
+	 numResourcesLoaded += 1; //이미지가 로드될때마다 키우고  
+	 if(numResourcesLoaded === totalResources) { //다 로드되면 다시그림
+		 setInterval(redraw, 1000/fps);
+	 }
+ }
+
+ function redraw() {
+	 var x = charX;
+	 var y = charY;
+	 
+	 canvas.width = canvas.width; //canvas초기화 
+	 context.drawImage(images["todomon-leg"], x, y+140);
+	 context.drawImage(images["todomon-body"], x, y - breathAmt);
+	 
+	 drawEllipse(x+56, y+40 - breathAmt, 30, curEyeHeight); //왼쪽눈 
+	 drawEllipse(x+77, y+40 - breathAmt, 30, curEyeHeight); //오른쪽눈 
+	 
+ }
+ 
+ function drawEllipse(centerX, centerY, width, height) {
+		
+	  context.beginPath();
+	  
+	  context.moveTo(centerX, centerY - height/2);
+	  
+	  context.bezierCurveTo(
+	    centerX + width/2, centerY - height/2,
+	    centerX + width/2, centerY + height/2,
+	    centerX, centerY + height/2);
+
+	  context.bezierCurveTo(
+	    centerX - width/2, centerY + height/2,
+	    centerX - width/2, centerY - height/2,
+	    centerX, centerY - height/2);
+	 
+	  context.fillStyle = "white";
+	  context.fill();
+	  context.closePath();	
+	  
+	  context.beginPath();
+	  context.moveTo(centerX - 4, centerY - height/4 );
+	  context.bezierCurveTo(
+	    centerX + width/4 - 4, centerY - height/4 ,
+	    centerX + width/4 - 4, centerY + height/4 ,
+	    centerX - 4, centerY + height/4 );
+	  context.bezierCurveTo(
+	    centerX - width/4 - 5, centerY + height/4 ,
+	    centerX - width/4 - 5, centerY - height/4 ,
+	    centerX - 5, centerY - height/4 );
+	
+	  context.fillStyle = "black";
+	  context.fill();
+	  context.closePath();	
+	}
+ var breathInc = 0.1;
+ var breathDir = 1;
+ var breathAmt = 0;
+ var breathMax = 3;
+ var breathInterval = setInterval(updateBreath, 200/fps);
+
+ function updateBreath() {
+	 if(breathDir === 1){ //들이마시기 
+		 breathAmt -= breathInc;
+		 if(breathAmt < -breathMax) { //max까지 숨 쉬면 방향 바꾼다. in/out. breathAmount조절 
+			 breathDir = -1;
+		 }
+	 } else { //내쉬기 
+		 breathAmt +=breathInc;
+		 if(breathAmt > breathMax) {
+			 breathDir =1;
+		 }
+	 }
+ }
+/*
+ var maxEyeHeight = 25;
+ var curEyeHeight = maxEyeHeight;
+ var eyeOpenTime = 0; //눈뜨고 있은 시간 
+ var timeBtwBlinks = 4000; //윙크 간격 
+ var blinkUpdateTime = 200; //윙크상태 업데이트된 지난 초 
+ var blinkTimer = setInterval(updateBlink, blinkUpdateTime);
+ 
+ function updateBlink() {
+	 eyeOpenTime += blinkUpdateTime;
+	 if(eyeOpenTime >=timeBtwBlinks) {
+		 blink();
+	 }
+ }
+ 
+ 
+ function blink() {
+	 curEyeHeight -= 1;
+	 if(curEyeHeight <=0) {
+		 eyeOpenTime = 0;
+		 curEyeHeight = maxEyeHeight;
+	 } else {
+		 setTimeout(blink, 10);
+	 }
+ }
+ */
+ var maxEyeHeight = 25;
+ var curEyeHeight = maxEyeHeight;
+ var eyeOpenTime = 0; //눈뜨고 있은 시간 
+ var timeBtwBlinks = 4000; //윙크 간격 
+ var blinkUpdateTime = 200; //윙크상태 업데이트된 지난 초 
+ var blinkTimer = setInterval(blink, 4000);
+ 
+// function updateBlink() {
+//	 eyeOpenTime += blinkUpdateTime;
+//	 if(eyeOpenTime >=timeBtwBlinks) {
+//		 blink();
+//	 }
+// }
+ 
+ 
+ function blink() {
+	 curEyeHeight -= 1;
+	 if(curEyeHeight <=0) {
+		 eyeOpenTime = 0;
+		 curEyeHeight = maxEyeHeight;
+	 } else {
+		 setTimeout(blink, 10);
+	 }
+ }
